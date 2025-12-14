6eaa0972a69c8b79fcff0dbf9b1dd6cfe17cd290
Priyansheechouhan <149784453+Priyansheechouhan@users.noreply.github.com>
2025-12-14 22:32:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ client = MongoClient(
 )
 
 # Optional but recommended: force connection check
-client.admin.command("ping")
+#client.admin.command("ping")
 
 db = client.netflix_db
 movies_collection = db.movies
@@ -93,3 +93,4 @@ def recommend():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
